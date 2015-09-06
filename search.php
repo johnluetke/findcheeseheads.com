8c6b78a5d4643b69195880787d2350ccfe4f4195
johnluetke <john@johnluetke.net>
2015-09-06 12:11:31 -0700

    Stricter input filtering

--- search.php
+++ search.php
@@ -2,7 +2,7 @@
 include("views/header.twig");
 
 if (isset($_GET['q'])) {
-    $q = filter_var($_GET['q'], FILTER_SANITIZE_STRING);
+    $q = filter_input(INPUT_GET, "q", FILTER_SANITIZE_ENCODED);
 
     $db = new PDO(sprintf("mysql:host=%s;dbname=%s", DATABASE_HOST, DATABASE_NAME), DATABASE_USER, DATABASE_PASS);
     $r = $db->query(sprintf("SELECT * FROM Place WHERE name LIKE '%s' OR address LIKE '%s' AND pending = 0;", "%".$q."%", "%".$q."%"));
