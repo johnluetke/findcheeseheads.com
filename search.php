bca19c17e12ff102646ff41556772cf14f7c2297
johnluetke <john@johnluetke.net>
2015-09-11 19:40:23 -0700

    Improving search

--- search.php
+++ search.php
@@ -2,12 +2,44 @@
 include("views/header.twig");
 include("views/navbar.twig");
 
+require("vendor/autoload.php");
+
+use \FindCheeseheads\API\Search;
+
 if (isset($_GET['q'])) {
     $q = filter_input(INPUT_GET, "q", FILTER_SANITIZE_STRING);
+    $c = filter_input(INPUT_GET, "c", FILTER_SANITIZE_STRING);
+    $c = strlen($c) == 0 ? DEFAULT_COUNTRY : $c;
+
+    list($codes, $cities) = Search::zipCodeSearch($c, $q, 1);
+    if (sizeof($codes) < 1) {
+        unset($codes);
+        unset($cities);
+    }
+    else {
+        $where = "";
+        foreach ($codes as $code) {
+            $where = sprintf("%s OR %s LIKE '%%%s%%'", $where, "address", $code);
+        }
+
+        foreach ($cities as $city) {
+            $where = sprintf("%s OR %s LIKE '%%%s%%'", $where, "address", $city);
+        }
+    }
 
     $db = new PDO(sprintf("mysql:host=%s;dbname=%s", DATABASE_HOST, DATABASE_NAME), DATABASE_USER, DATABASE_PASS);
-    $r = $db->query(sprintf("SELECT * FROM Place WHERE name LIKE '%s' OR address LIKE '%s' AND pending = 0;", "%".$q."%", "%".$q."%"));
+    $r = $db->query(sprintf("SELECT * FROM Place WHERE name LIKE '%s' OR address LIKE '%s' %s AND pending = 0;", "%".$q."%", "%".$q."%", $where));
     $results = $r->fetchAll();
+}
+
+function getZipCodeCoords($zip_code) {
+    $data = \Zippopotamus\Service\Zippopotamus::code(DEFAULT_COUNTRY, $zip_code);
+    $result = $data->places[0];
+    return array(
+        "city" => $result->{'place name'},
+        "lat"  => $result->latitude,
+        "lng"  => $result->longitude
+    );
 }
 ?>
 
@@ -16,12 +48,33 @@ if (isset($_GET['q'])) {
 </div>
 
 <div class="container">
-    <form id="search" action="search.php" method="get">
+    <form id="search" action="search.php" method="get" class="form-horizontal">
         <div class="form-group has-feedback">
-            <input type="text" class="form-control" name="q" placeholder="Search by name or address" value="<?php echo $_GET['q']; ?>" />
-            <i class="glyphicon glyphicon-search form-control-feedback"></i>
+            <div class="col-sm-8">
+                <input type="text" class="form-control" name="q" placeholder="Search by name or address" value="<?php echo $_GET['q']; ?>" />
+                <i class="glyphicon glyphicon-search form-control-feedback"></i>
+            </div>
+            <div class="col-sm-3">
+                <select class="form-control" name="c">
+                    <?php
+                    foreach (Search::getCountries() as $code=>$name) {
+                    ?>
+                        <option value="<?php echo $code;?>" <?php echo ($code == (isset($c) ? $c : DEFAULT_COUNTRY)) ? "selected" : "";?>><?php echo $name;?></option>
+                    <?php
+                        }
+                    ?>
+                </select>
+            </div>
+            <div class="col-sm-1">
+                <input type="submit" class="btn btn-primary form-control" value="Search"/a>
+            </div>
          </div>
     </form>
+    <?php if (isset($cities) && sizeof($cities) > 1) { ?>
+    <div class="container">
+        <p>Results include venues in <?php echo implode_list(", ", $cities);?>.</p>
+    </div>
+    <?php } ?>
 </div>
 
 <div class="container">
