e97891efcf78912b2fc7d662ea0e8e3037d632ad
johnluetke <john@johnluetke.net>
2017-01-14 10:21:47 -0800

    Add version

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
 require_once("config.php");
 require_once("vendor/autoload.php");
+require_once("src/php/api_auth.php");
 
 use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 use FindCheeseheads\App;
 use FindCheeseheads\Controllers\APIController;
 
@@ -15,10 +19,13 @@ $app->register(new \Silex\Provider\TwigServiceProvider(), array(
 
 $fc = new App();
 
-$app->get("/", function () use ($app, $fc) {
-    return $app['twig']->render("index.template.html", array(
+$app->get("/", function (Request $request) use ($app, $fc) {
+    $response = new Response($app['twig']->render("index.template.html", array(
         "version" => $fc->getVersion()
-    ));
+    )));
+
+    $response->headers->setCookie(fc_set_api_cookie($request));
+    return $response;
 });
 
 $app->mount("/api", new APIController());
