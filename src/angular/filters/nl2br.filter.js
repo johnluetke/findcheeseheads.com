b0d1f6569f0d4572e36d2336937df70a0c11ea85
John Luetke <johnl1479@gmail.com>
2017-12-29 11:00:49 -0600

    Refactor nl2br filter

--- src/angular/filters/nl2br.filter.js
+++
@@ -1,6 +0,0 @@
-angular.module("FindCheeseheadsApp").filter("nl2br", function($sce) {
-    return function(msg) { 
-        msg = msg.replace(/\r\n|\r|\n/g, "<br />");
-        return $sce.trustAsHtml(msg);
-    }
-});
